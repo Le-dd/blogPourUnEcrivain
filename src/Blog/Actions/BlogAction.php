94b84f228f72810a144013abef9d216c04d851fc
emeric <emeric.lebbrecht@gmail.com>
2018-05-11 18:42:56 +0200

    implement pdo for show.twig

--- src/Blog/Actions/BlogAction.php
+++ src/Blog/Actions/BlogAction.php
@@ -4,6 +4,8 @@ namespace App\Blog\Actions;
 use Framework\Renderer\RendererInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Framework\Renderer\TwigRendererFactory;
+use Framework\Router;
+use GuzzleHttp\Psr7\Response;
 
 class BlogAction{
 
@@ -17,16 +19,21 @@ class BlogAction{
    */
   private $pdo;
 
-  public function __construct(RendererInterface $renderer, \PDO $pdo )
+  /**
+   * @var Router
+   */
+  private $router;
+  public function __construct(RendererInterface $renderer, \PDO $pdo, Router $router )
   {
     $this->renderer = $renderer;
     $this->pdo = $pdo;
+    $this->router = $router;
   }
    public function __invoke(Request $request)
   {
-    $slug = $request->getAttribute('slug');
-    if($slug){
-      return $this->show($slug);
+
+    if($request->getAttribute('id')){
+      return $this->show($request);
 
     }
 
@@ -41,11 +48,31 @@ class BlogAction{
       ->fetchAll();
     return $this->renderer->render('@blog/index', compact('posts'));
   }
-
-  public function show(string $slug): string
+/**
+ * Affiche un article
+ * @param  Request $request
+ * @return ResponseInterface|string
+ */
+  public function show(Request $request)
   {
+    $slug = $request->getAttribute('slug');
+    $query = $this->pdo
+      ->prepare('SELECT * FROM post WHERE id = ?');
+    $query->execute([$request->getAttribute('id')]);
+    $post = $query->fetch();
+    
+    if ($post->slug !== $slug) {
+      $redirectUri = $this->router->generateUri('blog.show',[
+          'slug'=> $post->slug,
+          'id' => $post->id
+      ]);
+      return (new Response())
+        ->withStatus(301)
+        ->withHeader('location',$redirectUri);
+
+    }
     return $this->renderer->render('@blog/show', [
-      'slug' => $slug
+      'post' => $post
     ]);
   }
 }
