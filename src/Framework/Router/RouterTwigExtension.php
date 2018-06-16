508f34849251bd12818db8cba1d0b27f6b3c93f2
emeric <emeric.lebbrecht@gmail.com>
2018-06-16 11:05:51 +0200

    fixe menu active

--- src/Framework/Router/RouterTwigExtension.php
+++ src/Framework/Router/RouterTwigExtension.php
@@ -20,6 +20,7 @@ class RouterTwigExtension extends \twig_Extension{
     return[
       new \Twig_SimpleFunction('path', [$this,'pathFor']),
       new \Twig_SimpleFunction('is_subpath', [$this,'isSubPath']),
+      new \Twig_SimpleFunction('is_firstpath', [$this,'isFirstPath']),
       new \Twig_SimpleFunction('host_serveur', [$this,'hostServeur'])
     ];
   }
@@ -41,5 +42,17 @@ class RouterTwigExtension extends \twig_Extension{
       $uri = $_SERVER['REQUEST_URI'] ?? '/';
       $expectedUri = $this->router->generateUri($path);
       return strpos($uri, $expectedUri) !== false;
+  }
+
+  public function isFirstPath(string $path): bool
+  {
+      $uri = $_SERVER['REQUEST_URI'] ?? '/';
+      $expectedUri = $this->router->generateUri($path);
+      if($uri === $expectedUri){
+        $result = true;
+      }else{
+        $result = False;
+      }
+      return $result ;
   }
 }
